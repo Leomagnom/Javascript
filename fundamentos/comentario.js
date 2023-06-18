9d38701c88bd1267f67f27845e8af7ed71c442e3
Leomagnom <leomagnom@gmail.com>
2023-06-17 21:10:55 -0300

    primeiros códigos de javaScript

---
+++ fundamentos/comentario.js
@@ -0,0 +1,11 @@
+// console. log é usado para exibir algo na tela do computador!
+console.log('bom dia')
+console.log('boa tarde') // mais um exemplo de cosole. log!
+console.log('boa noite')
+/**
+ * Esse é um 
+ * comentário 
+ * de 
+ * múltiplas 
+ * linhas!
+ */
---
+++ fundamentos/sentenca_2.js
@@ -0,0 +1,1 @@
+console.log("Premeiros passo no javascritp");
