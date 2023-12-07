919cb4daeb10c0bbd31af0cde3f2c2da06ca0105
Leomagnom <leomagnom@gmail.com>
2023-12-06 21:36:21 -0300

    Primeiro  execicios: 1

---
+++ fundamentos/exercicios/exercicios1.js
@@ -0,0 +1,34 @@
+const cumprimentar= a => console.log(a)
+cumprimentar('Olá Leonardo')
+    
+const saudacoes= o => console.log(o)
+saudacoes('Olá Maria')
+
+const cumprimentar2=('Olá Lucimar!')
+function text() {
+    const saudacao= 'falaaa'
+    return saudacao
+}
+console.log(cumprimentar2, text())
+
+const cumprimentar3= a =>
+console.log(a) 
+cumprimentar3 ('Olá Sn Marinho')
+
+
+const converterIdadeEmAnosParaDias1= "Dias"
+let Anos = 25;
+let Dias = 365;
+let resultado1 = 25 * 365
+
+console.log(resultado1)
+
+const converterIdadeEmAnosParaDias2= "Dias"
+let Anos2 = 70;
+let Dias2 = 365;
+let resultado2 = 70 * 365
+
+console.log(resultado2)
+
+
+
