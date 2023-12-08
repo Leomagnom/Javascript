1e963f1508a1b7ec49aa9aba63414b9ab1e41468
Leomagnom <leomagnom@gmail.com>
2023-12-08 20:08:13 -0300

    exercicios2

---
+++ fundamentos/exercicios/exercicios2.js
@@ -0,0 +1,18 @@
+function cumprimentar(nome) {
+    const saudacao = "Olá!" 
+    return [saudacao, Leonardo].join(', ').concat("!")
+}
+console.log("Olá!")
+
+
+function cumprimentar(nome) {
+    return  ["Olá, Leonardo!"]
+  
+}
+
+console.log("Olá Leonardo!")
+
+function cumprimentar(nome) {
+  return `Olá, ${Leonardo}! `
+}
+console.log(`Olá Leonardo!`)
